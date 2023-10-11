7d442adfd0030569412f196e1a814670d7bdc0ad
Piotr Urbanski <piotr.s.urbanski@gmail.com>
2023-10-11 20:02:09 +0200

    Set up GameView in UI

---
+++ Assets/pindwin/Scripts/View/TileView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace pindwin.Scripts.View
+{
+	public class TileView : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
+	{
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			Debug.Log(gameObject.name);
+		}
+
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			Debug.Log(gameObject.name);
+		}
+	}
+}
